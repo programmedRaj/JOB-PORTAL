736fca2fc8bf78201c4354e6361e00a3de821eea
rajsh <rajshah9172@gmail.com>
2020-08-02 10:39:16 +0530

    few tweaks in paths

--- server/resumeocr.py
+++ server/resumeocr.py
@@ -14,47 +14,41 @@ def de_bhai_pdf(dia_kya):
     lam = ''
 
     for i in images:
-        pytesseract.pytesseract.tesseract_cmd = 'D:/Tesseract-OCR/tesseract.exe'
+        pytesseract.pytesseract.tesseract_cmd = 'D:/Tesseract-OCR/tesseract.exe' 
         text = pytesseract.image_to_string(i)
-        lam = lam+text
+        lam=lam+text
     if data['experience'] != None:
         print(data)
-        a = ''.join(data['experience']).split(".")
-        data['experience'] = a
-        #finder = Finder()
-        yay = []
-        dates = []
+        a=''.join(data['experience']).split(".")
+        data['experience']=a
+        #finder=Finder()
+        yay=[]
+        dates=[]
         for i in data['experience']:
 
-            matches = datefinder.find_dates(
-                i.replace('\xa0', ' ').replace('-', 'gibbrish '))
+            matches = datefinder.find_dates(i.replace('\xa0',' ').replace('-','gibbrish '))
             for match in matches:
                 dates.append(match.strftime('%b %Y'))
-            if len(dates) != 0:
-                me = ' '.join(i.split(' ')[:3])
-                #u = finder.findall(i.replace('\xa0', ' '))
-                a = {'Job title':'', 'Company name': me, 'dates': dates,
-                     'Description': ' '.join(i.replace('\xa0', '').split(' ')[10:])}
+            if len(dates)!=0:
+                me=' '.join(i.split(' ')[:3])
+                #u=finder.findall(i.replace('\xa0',' '))
+                a={'Job title':'','Company name':me,'dates':dates,'Description':' '.join(i.replace('\xa0','').split(' ')[10:])}
                 yay.append(a)
-            dates = []
+            dates=[] 
 
-        regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-        data['Education'] = ''.join(lam[lam.upper().find('EDUCATION')+10: lam.upper(
-        ).find('EDUCATION')+lam[lam.upper().find('EDUCATION')+12:].find('\n')])
-        data['Links'] = re.findall(regex, lam.replace('\n', ''))
-        data['experience'] = yay
-        yum = ['college_name', 'degree', 'designation',
-               'company_names', 'no_of_pages']
+        regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))" 
+        data['Education']=''.join(lam[lam.upper().find('EDUCATION')+10: lam.upper().find('EDUCATION')+lam[lam.upper().find('EDUCATION')+12:].find('\n')])
+        data['Links']=re.findall(regex,lam.replace('\n',''))
+        data['experience']=yay
+        yum=['college_name','degree','designation','company_names','no_of_pages']
         for i in yum:
             data.pop(i)
-    else:
-        regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-        data['Education'] = ''.join(lam[lam.upper().find('EDUCATION')+10: lam.upper(
-        ).find('EDUCATION')+lam[lam.upper().find('EDUCATION')+12:].find('\n')])
-        data['Links'] = re.findall(regex, lam.replace('\n', ''))
-        data['experience'] = 'none'
-        yum = ['college_name', 'degree', 'designation',
-               'company_names', 'no_of_pages']
+    else:        
+        regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))" 
+        data['Education']=''.join(lam[lam.upper().find('EDUCATION')+10: lam.upper().find('EDUCATION')+lam[lam.upper().find('EDUCATION')+12:].find('\n')])
+        data['Links']=re.findall(regex,lam.replace('\n',''))
+        data['experience']='none'
+        yum=['college_name','degree','designation','company_names','no_of_pages']
         for i in yum:
             data.pop(i)
-    return data
+    return data  
